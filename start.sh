87fdd553c0a379179b4c81295d8ec6956a1c6073
Nsnthkasturi <108393178+Nsnthkasturi@users.noreply.github.com>
2022-07-22 14:45:29 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [[ -n $RCLONE_CONFIG ]]; then
-	wget wget -P /app/.config/rclone $RCLONE_CONFIG
 	echo "Rclone config detected"
+	wget wget -P /app/.config/rclone $RCLONE_CONFIG
 	#echo "$(echo $RCLONE_CONFIG_BASE64|base64 -d)" >> rclone.conf
-   	 mkdir -p /app/.config/rclone
+    #mkdir -p /app/.config/rclone
 	#echo "[DRIVE]" > /app/.config/rclone/rclone.conf
 fi
 
